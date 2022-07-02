786f7ef2ff7bb5581f6803f997a9b84327684091
Houser97 <arturo.riverar97@gmail.com>
2022-07-02 15:26:22 -0500

    Style Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import '../styles/header.css';
+import logo from '../images/disney-logo.png';
 
 const Header = () => {
     return(
         <header>
             <div className='options-header'>
-                <div className='logo'></div>
+                <div className='logo'>
+                    <img className='img-logo' src = {logo} alt = "logo disney"></img>
+                </div>
                 <div className='home option'>HOME</div>
                 <div className='search option'>SEARCH</div>
                 <div className='watchlist option'>WATCHLIST</div>
