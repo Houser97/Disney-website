a1a524e4e8b034967ef96f22d30b5074e9a08932
Houser97 <arturo.riverar97@gmail.com>
2022-07-02 19:01:05 -0500

    Create movies state in Carousel component
    This state contains 5 objects with the content of the movies to be
    displayed in the Carousel component.

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,7 +1,19 @@
 import '../styles/carousel.css';
-import Obiwan from '../images/Obi-wan-poster.webp'
+import Obiwan from '../images/Obi-wan-poster.webp';
+import Daredevil from '../images/Daredevil.jpg';
+import DrStrange from '../images/Dr-Strange.webp';
+import Moonknight from '../images/Moon-knight.jpg';
+import Defenders from '../images/Defenders.webp';
+import { useState } from 'react';
 
 const Carousel = () => {
+
+    const [movies] = useState([{title: "Obi-Wan",   image: Obiwan,      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus libero ligula, ac ultrices eros sollicitudin pharetra. Mauris vitae convallis massa. Suspendisse potenti."},
+                               {title: "Daredevil", image: Daredevil,   desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus libero ligula, ac ultrices eros sollicitudin pharetra. Mauris vitae convallis massa. Suspendisse potenti."},
+                               {title: "Dr. Strange 2", image: DrStrange,   desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus libero ligula, ac ultrices eros sollicitudin pharetra. Mauris vitae convallis massa. Suspendisse potenti."},
+                               {title: "Moon Knight", image: Moonknight,   desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus libero ligula, ac ultrices eros sollicitudin pharetra. Mauris vitae convallis massa. Suspendisse potenti."},
+                               {title: "Defenders", image: Defenders,   desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus libero ligula, ac ultrices eros sollicitudin pharetra. Mauris vitae convallis massa. Suspendisse potenti."}])
+
     return(
         <div className='carousel-section'>
             <div className='carousel'>
