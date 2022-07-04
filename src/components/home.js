a1b8e4eb415c50433aca614061d671f56823a973
Houser97 <arturo.riverar97@gmail.com>
2022-07-03 22:27:54 -0500

    Add png images according to the section

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,12 @@
 import '../styles/home.css';
 import Carousel from './carousel';
+import ImageCategories from './imageCategories';
 
 const Home = () => {
     return(
         <div className='home'>
             <Carousel />
+            <ImageCategories />
         </div>
     )
 }
---
+++ src/components/imageCategories.js
@@ -0,0 +1,32 @@
+import '../styles/imageCategories.css';
+import Disney from '../images/Disney.png';
+import Pixar from '../images/Pixar.png';
+import Marvel from '../images/Marvel.png';
+import StarWars from '../images/Star.png';
+import National from '../images/National.png';
+
+const ImageCategories = () => {
+    return(
+        <div className='image-category-section'>
+            <div className='images-category-container'>
+                <div className='image-category'>
+                    <img src={Disney} alt = "Disney" className='image-png disney'></img>
+                </div>
+                <div className='image-category'>
+                    <img src={Pixar} alt = "Pixar" className='image-png pixar'></img>
+                </div>
+                <div className='image-category'>
+                    <img src={Marvel} alt = "Marvel" className='image-png'></img>
+                </div>
+                <div className='image-category'>
+                    <img src={StarWars} alt = "StarWars" className='image-png'></img>
+                </div>
+                <div className='image-category'>
+                    <img src={National} alt = "National" className='image-png'></img>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ImageCategories;
