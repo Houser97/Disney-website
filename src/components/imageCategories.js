61be1bb740ce0b772593d3f20e5cd3601690acdc
Houser97 <arturo.riverar97@gmail.com>
2022-07-04 13:34:59 -0500

    Change transition time and scale image section
    The transitions for al elements were changed to 250ms and given
    cubic bezier to improve the effects.

--- src/components/imageCategories.js
+++ src/components/imageCategories.js
@@ -7,23 +7,21 @@ import National from '../images/National.png';
 
 const ImageCategories = () => {
     return(
-        <div className='image-category-section'>
-            <div className='images-category-container'>
-                <div className='image-category'>
-                    <img src={Disney} alt = "Disney" className='image-png disney'></img>
-                </div>
-                <div className='image-category'>
-                    <img src={Pixar} alt = "Pixar" className='image-png pixar'></img>
-                </div>
-                <div className='image-category'>
-                    <img src={Marvel} alt = "Marvel" className='image-png'></img>
-                </div>
-                <div className='image-category'>
-                    <img src={StarWars} alt = "StarWars" className='image-png'></img>
-                </div>
-                <div className='image-category'>
-                    <img src={National} alt = "National" className='image-png'></img>
-                </div>
+        <div className='images-category-container'>
+            <div className='image-category'>
+                <img src={Disney} alt = "Disney" className='image-png disney'></img>
+            </div>
+            <div className='image-category'>
+                <img src={Pixar} alt = "Pixar" className='image-png pixar'></img>
+            </div>
+            <div className='image-category'>
+                <img src={Marvel} alt = "Marvel" className='image-png'></img>
+            </div>
+            <div className='image-category'>
+                <img src={StarWars} alt = "StarWars" className='image-png'></img>
+            </div>
+            <div className='image-category'>
+                <img src={National} alt = "National" className='image-png national'></img>
             </div>
         </div>
     )
