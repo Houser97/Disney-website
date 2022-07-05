a3e120570fb66e77b831d0d53868b6bea56d297a
Houser97 <arturo.riverar97@gmail.com>
2022-07-04 20:24:57 -0500

    Create Movies component

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,14 @@
 import '../styles/home.css';
 import Carousel from './carousel';
 import ImageCategories from './imageCategories';
+import Movies from './moviesSection';
 
 const Home = () => {
     return(
         <div className='home'>
             <Carousel />
             <ImageCategories />
+            <Movies />
         </div>
     )
 }
---
+++ src/components/moviesSection.js
@@ -0,0 +1,11 @@
+import '../styles/moviesSection.css';
+
+const Movies = () => {
+    return(
+        <div className='movies-series-section'>
+
+        </div>
+    )
+}
+
+export default Movies;
