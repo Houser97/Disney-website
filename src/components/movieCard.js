6b4b1db07036ab0924ebb8daff151e10d08f5706
Houser97 <arturo.riverar97@gmail.com>
2022-07-04 22:34:36 -0500

    Style movieCard component
    The Recommended component should display 4 image per slide, which
    will be accomplish in upcoming commits using react-slick. Base on that,
    each movieCard component has a width of 100%/4.

--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -1,9 +1,10 @@
 import '../styles/movieCard.css';
 
-const MovieCard = () => {
+const MovieCard = ({movie}) => {
+    const image = movie.image;
     return(
         <div className='movie-card'>
-
+            <img src= {image} alt = "Daredevil" className='image-movie-card'></img>
         </div>
     )
 }
--- src/components/recommended.js
+++ src/components/recommended.js
@@ -1,10 +1,36 @@
+import { useState } from 'react';
 import '../styles/recommended.css';
 import MovieCard from './movieCard';
+import Daredevil from '../images/movies/DaredevilSerie.JPG';
+import Malcolm from '../images/movies/MalcolmSerie.JPG';
+import Luca from '../images/movies/Luca.JPG';
+import ObiWan from '../images/movies/ObiSerie.JPG';
+import Rio from '../images/movies/RioSerie.JPG';
+import Luke from '../images/movies/LukeSerie.JPG'
 
 const Recommended = () => {
+
+    const [recommendedMovies] = useState([{image: Daredevil},
+                                          {image: Luca},
+                                          {image: Malcolm},
+                                          {image: ObiWan},
+                                          {image: Rio},
+                                          {image: Luke}])
+
     return(
         <div className='container-movies'>
-            <MovieCard />
+            <div className='title-section'>Recommended for you</div>
+            <div className='images-section-movies'>
+                {
+                    recommendedMovies.map(function iterateMovies(movie, i){
+                        return(
+                            <MovieCard key={i} movie = {movie} />
+                        )
+                    })
+                }
+                
+            </div>
+            
         </div>
     )
 }
