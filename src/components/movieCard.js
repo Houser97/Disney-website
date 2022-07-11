1e1c10712e1669c0812dda87b7968ce73885d4ff
Houser97 <arturo.riverar97@gmail.com>
2022-07-11 12:28:37 -0500

    Add add to watchlist button to movieCard component

--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -14,7 +14,12 @@ const MovieCard = ({movie}) => {
             {
                 userID !== null ? (
                     <div className='add-icon-movieCard'>
-                        icon
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 120 120">
+                            <g fill="none" fill-rule="evenodd" opacity=".2">
+                                <circle cx="60" cy="60" r="57.5" stroke="#FFF" stroke-width="5"/>
+                                <path fill="#F9F9F9" d="M63.626 57.254h17.739a3.5 3.5 0 0 1 3.5 3.5v.434a3.5 3.5 0 0 1-3.5 3.5h-17.74v17.739a3.5 3.5 0 0 1-3.5 3.5h-.433a3.5 3.5 0 0 1-3.5-3.5v-17.74H38.453a3.5 3.5 0 0 1-3.5-3.5v-.433a3.5 3.5 0 0 1 3.5-3.5h17.74V39.515a3.5 3.5 0 0 1 3.5-3.5h.433a3.5 3.5 0 0 1 3.5 3.5v17.739z"/>
+                            </g>
+                        </svg>
                     </div>
                 ) : (
                     <div className='add-icon-userID-null'>
